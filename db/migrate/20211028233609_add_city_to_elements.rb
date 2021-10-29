46bf4923a8ec014e6b7bcd569243472ac9864390
tprat <Titouan.p@hotmail.fr>
2021-10-29 02:00:37 +0200

    first commit

---
+++ db/migrate/20211028233609_add_city_to_elements.rb
@@ -0,0 +1,6 @@
+class AddCityToElements < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :dog_sitters, :city, foreign_key: true
+    add_reference :dogs, :city, foreign_key: true
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Dog.destroy_all
+DogSitter.destroy_all
+Stroll.destroy_all
+City.destroy_all
+
+5.times { City.create(city_name: Faker::Address.city) }
+
+5.times { DogSitter.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, city_id: City.all.sample.id) }
+5.times { Dog.create(name: Faker::Creature::Dog.name, city_id: City.all.sample.id) }
